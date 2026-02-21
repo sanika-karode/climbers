c79b5d6050403d81d6e5faefc97fbfde03ce8686
Sanika Karode <sanika.karode06@gmail.com>
2026-02-20 22:34:13 -0500

    Updated climbing app display styling

---
+++ climbing-app/src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <>
+      <h1 className="app-title">
+        ClimbAI – Optimal Route Analyzer
+      </h1>
+
+      <div className="upload-container">
+        <p>Upload your climbing wall image</p>
+
+        <input type="file" accept="image/*" />
+
+        <br />
+
+        <button className="upload-button">
+          Analyze Route
+        </button>
+      </div>
+
+      <div className="route-display">
+        <div className="route-path" />
+        <div className="climber-dot" />
+      </div>
+    </>
+  );
+}
+
+export default App;
