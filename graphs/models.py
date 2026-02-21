ada3ec39428965d602684d04cf500d86c60e7fb1
Ruhi-P <patelruhi06@gmail.com>
2026-02-21 10:13:33 -0500

    graph created with state(LH, RH) as node

---
+++ graphs/models.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass
+from typing import List, Tuple
+
+@dataclass
+class Hold:
+    id: int
+    x: float
+    y: float
+    hold_type: str #turn into enum later
+    role: str #turn into enum later
+
+@dataclass
+class Climber:
+    height_cs: float
